c2c44fa99361334ca6ec9ffff1f5d33317acd823
Santosh <santhosht@baylogictech.com>
2019-08-13 17:00:39 +0530

    Added pattern attributes through script

--- images/dotcms/ROOT/srv/92-haproxy-config.sh
+++ images/dotcms/ROOT/srv/92-haproxy-config.sh
@@ -6,10 +6,9 @@ source /srv/utils/discovery-include.sh
 source /srv/utils/config-defaults.sh
 
 echo "Haproxy config ...."
-
 if [[ -z "${CMS_DNSNAMES}" ]]; then
 
-    echo "Haproxy service not detected" 
+    echo "CMS_VALVE_IP=${CMS_VALVE_IP}" >>/srv/config/settings.ini
 
 else
 	sleep ${PROVIDER_HAPROXY_SVC_DELAY_MIN}
@@ -22,6 +21,8 @@ else
 		for haproxy_candidate_ip in "${haproxy_ip_list[@]}"; do
 			if dockerize -wait "tcp://${haproxy_candidate_ip}:${PROVIDER_HAPROXY_PORT_HTTP}" -timeout ${PROVIDER_HAPROXY_SVC_DELAY_STEP}s true 2&>1 >/dev/null; then
 				echo "${CMS_DNSNAMES} live at IP ${haproxy_candidate_ip}"
+				: ${CMS_VALVE_IP:="%a %A"}
+				echo "CMS_VALVE_IP=${CMS_VALVE_IP}" >>/srv/config/settings.ini
 	            break 2
 			else 
 				echo "${CMS_DNSNAMES} not live"
