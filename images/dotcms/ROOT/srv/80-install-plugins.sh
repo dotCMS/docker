d2273d78abe9f9f205b1f3d0693168938669df8a
Santosh <santhosht@baylogictech.com>
2019-08-20 15:24:00 +0530

    Added condition to detect static plugins

--- images/dotcms/ROOT/srv/80-install-plugins.sh
+++ images/dotcms/ROOT/srv/80-install-plugins.sh
@@ -13,13 +13,15 @@ fi
 
 echo "Installing static plugins with JAVA_HOME=${JAVA_HOME}"
 if [[ -d /plugins/static ]]; then
-    echo "Found $(find /plugins/static/ -mindepth 1 -maxdepth 1 -type d | wc -l) plugins"
-    mv /srv/plugins/common.xml /tmp/plugins-common.xml
-    mv /srv/plugins/plugins.xml /tmp/plugins-plugins.xml
-    cp -a /plugins/static/* /srv/plugins/
-    cp /tmp/plugins-common.xml /srv/plugins/common.xml 
-    cp /tmp/plugins-plugins.xml /srv/plugins/plugins.xml 
+	if [[ $(find /plugins/static/ -mindepth 1 -maxdepth 1 -type d | wc -l) -gt 0 ]]; then
+	    echo "Found $(find /plugins/static/ -mindepth 1 -maxdepth 1 -type d | wc -l) plugins"
+	    mv /srv/plugins/common.xml /tmp/plugins-common.xml
+	    mv /srv/plugins/plugins.xml /tmp/plugins-plugins.xml
+	    cp -a /plugins/static/* /srv/plugins/
+	    cp /tmp/plugins-common.xml /srv/plugins/common.xml 
+	    cp /tmp/plugins-plugins.xml /srv/plugins/plugins.xml 
 
-    cd /srv && ./bin/deploy-plugins.sh || exit 1
+	    cd /srv && ./bin/deploy-plugins.sh || exit 1
+	fi    
 fi
 
