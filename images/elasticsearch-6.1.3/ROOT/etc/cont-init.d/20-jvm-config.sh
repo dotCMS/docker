93453d4f3c94f5231864059b85bc0902f3091b0b
Brent Griffin <griffin.brent@gmail.com>
2019-08-26 22:11:26 +0000

    merged in changes to support openshift as an orchestrator

--- images/elasticsearch-6.1.3/ROOT/etc/cont-init.d/20-jvm-config.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/with-contenv /bin/bash
-
-set -e
-
-source /srv/container-utils/config-defaults.sh
-
-export PROVIDER_ELASTICSEARCH_HEAP_SIZE
-
-dockerize -template /srv/config/jvm.options:/srv/config/jvm.options
