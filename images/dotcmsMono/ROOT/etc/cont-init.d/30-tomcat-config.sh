bb902b352cb91975ab70324257f66dee1ae65b1a
Will Ezell <will@dotcms.com>
2019-11-01 10:44:17 -0400

    removing mono docker - will implement elsewhere

--- images/dotcmsMono/ROOT/etc/cont-init.d/30-tomcat-config.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/with-contenv /bin/bash
-
-set -e
-
-source /srv/docker-container/utils/config-defaults.sh
-
-
-echo "CMS_CONNECTOR_THREADS=${CMS_CONNECTOR_THREADS}" >>/srv/docker-container/config/settings.ini
-echo "CMS_SMTP_HOST=${CMS_SMTP_HOST}" >>/srv/docker-container/config/settings.ini
